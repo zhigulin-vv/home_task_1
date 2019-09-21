603548e59fd1a38c9c0b02b2a0bed475d59521cf
zhigulin-vv <Qq.123456>
2019-09-21 14:01:30 +0300

    Всякое промежуточное

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
 
 
+        System.out.println(initRange(args[0]));
 
 
         //Создаем запись в журнале
@@ -32,6 +33,69 @@ public class Main {
         time_elapsed.append("мс.");
 
         return time_elapsed.toString();
+
+    }
+
+    public static boolean initBefore(String settings)
+    {
+
+        try
+        {
+            Integer.parseInt(settings);
+            return true;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+
+    }
+
+    public static String initRange(String income)
+    {
+
+        StringBuilder Fibs = new StringBuilder();
+
+        if (initBefore(income))
+        {
+            //Выводим ряд чисел Фибоначчи
+            Fibs.append(getFibs(Integer.parseInt(income)));
+
+        }
+        else
+        {
+            Fibs.append("С входнными данными '");
+            Fibs.append(income);
+            Fibs.append("' невозможно произвести рассчет.");
+        }
+
+        return Fibs.toString();
+
+    }
+
+
+    public static String getFibs(int it)
+    {
+
+        StringBuilder Fibs = new StringBuilder();
+
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        Fibs.append(n0+" "+n1+" ");
+
+        for(int i = 3; i <= it ; i++){
+
+            n2=n0+n1;
+
+            Fibs.append(n2+" ");
+
+            n0=n1;
+            n1=n2;
+
+        }
+
+        return Fibs.toString();
 
     }
 
